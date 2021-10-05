e2f4b25dbbc33f0f5077ca6e8bae697d46721655
binggo <15524215898@163.com>
2021-10-05 12:05:34 +0800

    first commit

---
+++ src/test/java/com/imooc/SellApplicationTests.java
@@ -0,0 +1,15 @@
+package com.imooc;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
