325a438f306642f90d970c184bb2d18121b22736
binggo <15524215898@163.com>
2021-10-05 12:03:41 +0800

    first commit

---
+++ src/main/java/com/imooc/dataobject/ProductCategory.java
@@ -0,0 +1,41 @@
+package com.imooc.dataobject;
+
+import lombok.Data;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+@Data
+@DynamicUpdate//动态更新
+public class ProductCategory {
+    /**类目id */
+    @Id
+    @GeneratedValue
+    private Integer categoryId;
+
+    /**
+     * 类目名字
+     */
+    private String categoryName;
+
+    /**
+     * 类目编号
+     */
+    private Integer categoryType;
+
+    private Date updateTime;
+
+    private Date createTime;
+
+    public ProductCategory() {
+    }
+
+    public ProductCategory(String categoryName, Integer categoryType) {
+        this.categoryName = categoryName;
+        this.categoryType = categoryType;
+    }
+}
